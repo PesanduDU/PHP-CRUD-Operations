d8ea235729826c1212f0f4ba6754a73befdad4bd
PesanduDU <pesandudubeysekara@gmail.com>
2024-10-15 00:39:43 +0530

    3rd commit

---
+++ display.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Users</title>
+
+    <link rel="stylesheet" href="styles.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container my-5">
+
+        <button class="c-button">
+            <span class="c-main">
+                <span class="c-ico"><span class="c-blur"></span> <span class="ico-text">+</span></span>
+                Add User
+            </span>
+        </button>
+
+
+    </div>
+
+</body>
+
+</html>
